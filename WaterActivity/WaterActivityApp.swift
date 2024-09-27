682b7d05d211acd76e0242bd3938486baa971dbd
MayTheStar <maibadr200@gmail.com>
2024-09-27 18:45:39 +0300

    Initial Commit

---
+++ WaterActivity/WaterActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterActivityApp.swift
+//  WaterActivity
+//
+//  Created by May Bader Alotaibi on 24/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct WaterActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
